9655568c74ad363db2e768ca8875ad46ea7c716b
Eric Fan <ericfan2770@gmail.com>
2020-03-25 22:24:13 -0700

    2D Lists

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-names = ["John", "Bob", "Mosh", "Sarah", "Mary"]
-print(names)
-print(names[-2])
-print(names[2:])
-print(names[2:4])
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
-names[0] = "Jon"
-print(names)
-
-# String manipulation and array manipulation are pretty much the same!
-
+print(matrix)
+print(matrix[1])
+print(matrix[0][2])
 print()
 
-# Exercise
-numbers = [3, 2, 7, 4, 9]
-max = numbers[0]  # but this assumes nums is not empty
-for number in numbers:
-    if number > max:
-        max = number
-print(max)
+# matrix[0][2] = 20
+# print(matrix[0][2])
+
+for row in matrix:
+    for col in row:
+        print(col)
