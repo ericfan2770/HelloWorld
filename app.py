c14109b3fd9ccfda9201b153abda7549968d5630
Eric Fan <ericfan2770@gmail.com>
2020-03-25 21:43:06 -0700

    Car Game

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-secret_number = 9;
-guess_count = 0
-guess_limit = 3
+command = input("> ")
+is_start = False
 
-while guess_count < guess_limit:
-    guess = int(input("Guess: "))
-    guess_count += 1
-    if guess == secret_number:
-        print("You won!")
-        break  # immediately terminates loop
-else:
-    print("Sorry, you failed!")
+while command.lower() != "quit":
+    if command == "help":
+        print("""
+start - to start the car")
+stop - to stop the car
+quit - to exit
+        """)
+    elif command == "start":
+        if is_start:
+            print("The car is already started!")
+        else:
+            print("Car started...Ready to go!")
+            is_start = True
+    elif command == "stop":
+        if not is_start:
+            print("The car is already stopped!")
+        else:
+            print("Car stopped.")
+            is_start = False
+    else:
+        print("I don't understand that...")
+    command = input("> ")
+    command.lower()
+
