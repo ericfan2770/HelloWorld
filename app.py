be2372c926ade251e6b7728a4e565e8368e2eb66
Eric Fan <ericfan2770@gmail.com>
2020-03-26 12:20:24 -0700

    Parameters

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # function: container with a few lines of code for a specific purpose
-def greet_user():
-    print('Hi there!')
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}!")
     print('Welcome aboard')
 
 
 print("Start")
-greet_user()  # must be called after the function is defined
+greet_user("Eric", "Fan")  # must be called after the function is defined
 print("Finish")
