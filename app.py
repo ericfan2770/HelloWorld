942c9874639d8ad4d2c30934cdaa476c87c25fc9
Eric Fan <ericfan2770@gmail.com>
2020-03-25 22:58:24 -0700

    Dictionaries

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
-coordinates = (1, 2, 3)  # (x, y, z)
-#  coordinates[0] * coordinates[1] * coordinates[2]
-x = coordinates[0]
-y = coordinates[1]
-z = coordinates[2]
+# Customer: name, email, phone #, etc.
+# Name: John Smith
+# Email: john@gmail.com
+# Phone: 1234
 
-print(x * y * z)
-print()
+# Dictionary -- keys must be unique
+customer = {
+    "name": "John Smith",
+    "age": 30,
+    "is_verified": True
+}
+print(customer["name"])
+print(customer.get("name"))  # non-existent key is ok
 
-# Unpacking -- performs same action as lines 3-5, and much shorter and concise!
-# Works with lists and tuples
-x, y, z = coordinates
-print(x)
-print(y)
-print(z)
+print(customer.get("birthday"))  # none, which is like Java null
+print(customer.get("birthday", "Jan 1 1980"))  # default values
+
+customer["name"] = "Jack Smith"
+print(customer["name"])
+
+# Exercise
+numbers = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "0": "Zero"
+}
+
+phone = input("Phone: ")
+result = ""
+# for index in range(len(phone)):
+#     result += numbers.get(phone[index]) + " "
+for ch in phone:
+    result += numbers.get(ch, "Not a number!") + " "
+result = result[:len(result) - 1]
+print(result)
