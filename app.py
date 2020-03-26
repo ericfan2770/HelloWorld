f27227eaa699afd9b83d627af249abdff28f2eb0
Eric Fan <ericfan2770@gmail.com>
2020-03-25 21:51:10 -0700

    For Loops

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
-command = input("> ")
-is_start = False
+for item in 'Python':
+    print(item)
 
-while command.lower() != "quit":
-    if command == "help":
-        print("""
-start - to start the car")
-stop - to stop the car
-quit - to exit
-        """)
-    elif command == "start":
-        if is_start:
-            print("The car is already started!")
-        else:
-            print("Car started...Ready to go!")
-            is_start = True
-    elif command == "stop":
-        if not is_start:
-            print("The car is already stopped!")
-        else:
-            print("Car stopped.")
-            is_start = False
-    else:
-        print("I don't understand that...")
-    command = input("> ")
-    command.lower()
+print()
 
+# for item in ["Eric", "John", "Sarah"]:
+#     print(item)
+
+# for item in [1, 2, 3, 4, 5]:
+#      print(item)
+
+for item in range(10):  # range function, [0, 9]
+    print(item)
+
+print()
+
+for item in range(5, 10):
+    print(item)
+
+print()
+
+for item in range(5, 9, 2):  # optionally takes a step
+    print(item)
+
+print()
+
+prices = [10, 20, 30]
+total_price = 0
+for price in prices:
+    total_price += price
+print(f"Total: {total_price}")
