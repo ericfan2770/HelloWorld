a6a65b68fc8a2bcbcc09139523779383cf91fc35
Eric Fan <ericfan2770@gmail.com>
2020-03-25 22:43:46 -0700

    Unpacking

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-# Tuples are immutable
-numbers = (1, 2, 3)  # this is a tuple (read-only)
-# numbers[0] = 1
-print(numbers[0])
+coordinates = (1, 2, 3)  # (x, y, z)
+#  coordinates[0] * coordinates[1] * coordinates[2]
+x = coordinates[0]
+y = coordinates[1]
+z = coordinates[2]
+
+print(x * y * z)
+print()
+
+# Unpacking -- performs same action as lines 3-5, and much shorter and concise!
+# Works with lists and tuples
+x, y, z = coordinates
+print(x)
+print(y)
+print(z)
