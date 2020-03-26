4aa94b724719cb61d72a0a8d2208bede7b451199
Eric Fan <ericfan2770@gmail.com>
2020-03-25 21:55:09 -0700

    Nested Loops sdf quit

--- app.py
+++ app.py
@@ -1,31 +1,3 @@
-for item in 'Python':
-    print(item)
-
-print()
-
-# for item in ["Eric", "John", "Sarah"]:
-#     print(item)
-
-# for item in [1, 2, 3, 4, 5]:
-#      print(item)
-
-for item in range(10):  # range function, [0, 9]
-    print(item)
-
-print()
-
-for item in range(5, 10):
-    print(item)
-
-print()
-
-for item in range(5, 9, 2):  # optionally takes a step
-    print(item)
-
-print()
-
-prices = [10, 20, 30]
-total_price = 0
-for price in prices:
-    total_price += price
-print(f"Total: {total_price}")
+for x in range(4):  # [0, 3]
+    for y in range(4):
+        print(f"({x}, {y})")
