809dbe4b46cd6a0e3b3197e2014f8684c45cebfb
Eric Fan <ericfan2770@gmail.com>
2020-03-26 12:39:13 -0700

    Reusable Functions

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-# by default, functions return None
-def square(number):
-    print(number * number)
+def emoji_converter(message):
+    words = message.split(" ")
+    emojis = {
+        ":)": "🙂",
+        ":(": "😞"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
 
 
-print(square(3))
+message = input("> ")
+print(emoji_converter(message))
