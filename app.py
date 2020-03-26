f14f2608b0c7647f220cf4872557d0ff232dd53c
Eric Fan <ericfan2770@gmail.com>
2020-03-25 20:51:53 -0700

    If Statements

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-# Python module: separate file with reusable code
-import math  # import math module
+is_hot = False
+is_cold = False
 
-x = 2.9
-print(round(x))  # 3
+# Python scope is determined by indentation!
+# And Python execution is entirely line based, not semicolon
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day")
+print("Enjoy your day")
 
-print(abs(-2.9))
-
-print(math.ceil(2.9))
-print(math.floor(2.9))
+# Exercise
+price = 1000000
+has_good_credit = True
+if has_good_credit:
+    down_payment = price * 0.1
+else:
+    down_payment = price * 0.2
+print(f"Down payment: ${down_payment}")
