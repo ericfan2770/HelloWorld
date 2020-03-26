e8569667bf3b7a88ec8d6d51be0a15de945759e2
Eric Fan <ericfan2770@gmail.com>
2020-03-26 12:26:58 -0700

    Keyword Arguments

--- app.py
+++ app.py
@@ -5,5 +5,15 @@ def greet_user(first_name, last_name):
 
 
 print("Start")
-greet_user("Eric", "Fan")  # must be called after the function is defined
+greet_user("Fan", "Eric")  # positional arguments: their position/order matters
 print("Finish")
+
+print()
+
+# keyword arguments: their order does NOT matter
+greet_user(last_name="Fan", first_name="Eric")  # keyword arguments, helps with readability sometimes
+
+# calc_cost(total=50, shipping=5, discount=0.1)  # can be difficult to read
+
+# greet_user(first="John", "Smith") cannot mix keyword and positional args unless keyword is after positional args
+greet_user("John", last_name="Smith")
