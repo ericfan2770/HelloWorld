9cc2c320c658fb828e55b7e364af3ed9d5303a24
Eric Fan <ericfan2770@gmail.com>
2020-03-25 20:00:47 -0700

    Type Conversion

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-name = input("What is your name? ")
-favorite_color = input("What is your favorite color? ")
-print(name + " likes " + favorite_color);
+birth_year = input("Birth year: ") # return value is always a String
+print(type(birth_year))
+age = 2019 - int(birth_year) # converts birth_year from a String to an integer
+print(type(age))
+print(age)
+
+weight_pounds = input("What is your weight in pounds? ")
+weight_kilograms = int(weight_pounds) / 2.205
+print(weight_kilograms)
+
+# Conversions to other types
+# int()
+# float()
+# bool()
