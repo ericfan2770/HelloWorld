53ca76f033c7b3257644f199a70f029cbeaa6bd2
Eric Fan <ericfan2770@gmail.com>
2020-03-25 19:50:21 -0700

    User Input

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-price = 10
-rating = 4.9
-name = "Eric"
-is_published = True
-print(price)
-
-full_name = "John Smith"
-age = 20
-is_new = True
+name = input("What is your name? ")
+favorite_color = input("What is your favorite color? ")
+print(name + " likes " + favorite_color);
