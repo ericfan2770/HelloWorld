4100fc180e3f3365b25d2faa64909649d0a9c0dd
Eric Fan <ericfan2770@gmail.com>
2020-03-25 22:20:15 -0700

    Lists

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-numbers = [5, 2, 5, 2, 2]
+names = ["John", "Bob", "Mosh", "Sarah", "Mary"]
+print(names)
+print(names[-2])
+print(names[2:])
+print(names[2:4])
 
-for num in numbers:
-    stars = ""
-    for index in range(num):
-        stars += "*"
-    print(stars)
+names[0] = "Jon"
+print(names)
+
+# String manipulation and array manipulation are pretty much the same!
+
+print()
+
+# Exercise
+numbers = [3, 2, 7, 4, 9]
+max = numbers[0]  # but this assumes nums is not empty
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
